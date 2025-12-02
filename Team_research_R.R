a95265a3b89c679e8566a3db27b5e9f0f093a8d1
YUV RAJ (24139265) <zy25abb@herts.ac.uk>
2025-12-02 18:15:08 +0000

    Summary and other details od bank data.

--- Team_research_R.R
+++ Team_research_R.R
@@ -3,3 +3,11 @@ View(BankChurners_data)
 
 working_data <- BankChurners_data
 View(working_data)
+
+summary(working_data)
+
+colnames(working_data)  
+working_data$Attrition_num <- ifelse(working_data$Attrition_Flag == "Attrited Customer", 1, 0)
+
+cat("Dataset Preview:\n")
+print(head(working_data))
