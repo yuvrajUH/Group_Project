fde2473d57c9055b95d867377e5ca3ac7364b86d
YUV RAJ (24139265) <zy25abb@herts.ac.uk>
2025-12-02 17:38:43 +0000

    second commit

---
+++ Team_research_R.R
@@ -0,0 +1,5 @@
+BankChurners_data <- read.csv("https://raw.githubusercontent.com/yuvrajUH/Group_Project/refs/heads/main/group%20project%20TR/BankChurners.csv")
+View(BankChurners_data)
+
+working_data <- BankChurners_data
+View(working_data)
