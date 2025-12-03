c7756274244758981691b0f4247da61551cf3220
YUV RAJ (24139265) <zy25abb@herts.ac.uk>
2025-12-03 10:42:35 +0000

    plotting

--- Team_research_R.R
+++ Team_research_R.R
@@ -7,6 +7,8 @@ View(working_data)
 working_data$Attrition_Num <- ifelse(working_data$Attrition_Flag == "Attrited Customer", 1, 0)
 cat("\nSummary of Total Transaction Count:\n")
 print(summary(working_data$Total_Trans_Ct))
+
+#box plot
 boxplot(Total_Trans_Ct ~ Attrition_Flag,
         data = working_data,
         col = c("lightgreen", "salmon"),
@@ -14,4 +16,15 @@ boxplot(Total_Trans_Ct ~ Attrition_Flag,
         xlab = "Customer Status",
         ylab = "Total Transaction Count")
 
-#new Line of code 
+
+print(summary(working_data$Total_Trans_Ct))
+
+# Histogram
+hist(working_data$Customer_Age,
+     breaks = 10,
+     col = "lightblue",
+     border = "black",
+     main = "Histogram of Total Transaction Count",
+     xlab = "Customer Age group",
+     ylab = "Frequency")
+
